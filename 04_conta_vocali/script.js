f5b71875b3ce1887ffbe46ac272f64253012e845
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-03 18:52:39 +0200

    04- conta vocali: creato ciclo for, condizione con "includes" e "push"

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,31 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(parolaDaControllare) {
+
+    let vocaliTrovate = [];
+
+    for (let i = 0; i < parolaDaControllare.length; i++) { //avvio un ciclo for così guardo tutte le lettere una per una
+
+        const letteraCheStoGuardando = parolaDaControllare [i]; //prendo la lettera corrente/che sto guardando della parola alla posizione i
+        // al primo giro currentLetter = "j", al secondo = "a", poi "v", ecc
+
+        if ("aeiou".includes(letteraCheStoGuardando)) {
+
+             vocaliTrovate.push(letteraCheStoGuardando);
+        }
+    }
+
+    return vocaliTrovate;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const risultato = contaVocali(word);
 
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log("Vocali trovate:", risultato);
+console.log("Numero di vocali:", risultato.length);
