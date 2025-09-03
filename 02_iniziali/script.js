067b245a1540e9a42c04ea27272626271e447846
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-03 16:48:57 +0200

    02_iniziali: creato seconda versione con una funzione

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,9 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getInitials(arrayOfNames) {
+
+    let initials = [];
+
+    for (let i = 0; i < arrayOfNames.length; i++) {
+
+    const firstLetter = arrayOfNames [i] [0]; //prendo la prima lettera del nome
+
+    initials.push(firstLetter); //la aggiungo nell'array delle iniziali creato sopra 
+}
+
+    return initials; //consegno il nuovo array
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = getInitials(names);
+console.log("risultato con funzione", result);
 
 
 
@@ -17,16 +33,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 //METODO CON IL CICLO FOR:
 
-//creo array vuoto
-let initials = [];
+// // //creo array vuoto
+// let initials = [];
 
-//ciclo for
-for (let i = 0; i < names.length; i++) {
-    const firstLetter = names [i] [0]; //prendo la prima lettera del nome
-    initials.push(firstLetter); //la aggiungo nell'array delle iniziali creato sopra 
-}
+// // //ciclo for
+// for (let i = 0; i < names.length; i++) {
+//     const firstLetter = names [i] [0]; //prendo la prima lettera del nome
+//     initials.push(firstLetter); //la aggiungo nell'array delle iniziali creato sopra 
+// }
 
-//stampo a schermo il risultato atteso: ["A", "L", "M", "A", "G", "A"]
-console.log(initials);
+// //stampo a schermo il risultato atteso: ["A", "L", "M", "A", "G", "A"]
+// console.log("risultato solo con ciclo for senza funzione", initials);
 
 
