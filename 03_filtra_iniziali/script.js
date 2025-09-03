77a9c287b71a93bec98ed1390bfebcd93c5e2904
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-03 17:45:17 +0200

    03_ filtra iniziali: utilizzato funzione con ciclo for e condizione (if)

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterNamesByInitial(namesArray) {
+
+    let filteredNames = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+
+        const currentName = namesArray [i];
+
+        if (currentName[0] === "A") {
+
+            filteredNames.push(currentName);
+        }
+    }
+
+    return filteredNames;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = filterNamesByInitial(names);
 
-
+console.log("Nomi che iniziano per lettera A:", result);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
